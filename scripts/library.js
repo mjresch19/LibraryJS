55c378b113c8a0f890b251d2f01d4905fd952a3b
mjresch19 <93208304+mjresch19@users.noreply.github.com>
2024-06-30 22:14:32 -0400

    Added files via upload
    Template for a miniature project practicing with JS features

---
+++ scripts/library.js
@@ -0,0 +1,22 @@
+function Book(title,author,pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+
+        if (this.read) {
+            return this.title + " by " + this.author + ", " + this.pages + " pages, read." 
+        }
+
+        else {
+            return this.title + " by " + this.author + ", " + this.pages + " pages, not read yet."
+        }
+    }
+
+}
+
+function addBookToLibrary() {
+
+}
