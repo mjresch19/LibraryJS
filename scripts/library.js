8ef842df4a6bbcd63c9c29e68df6a54909c65094
reschmax <reschmax@msu.edu>
2024-07-02 17:15:00 -0400

    Added TODO list. Added functions for displying each book in the html and adding new books.

--- scripts/library.js
+++ scripts/library.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
-
+let cardContainer = document.getElementById("card-container");
+console.log(cardContainer);
 //Constructor for a book
 function Book(title,author,pages, read) {
     this.title = title;
@@ -22,11 +23,52 @@ function Book(title,author,pages, read) {
 
 //Function to pushing to the library array
 function addBookToLibrary(book) {
-    myLibrary.push(book)
+    myLibrary.push(book);
 }
+
+//Function to display books
+function displayBooks() {
+
+    //for loop that iterates through the obj array
+    myLibrary.forEach((book, i) => {
+
+        let newCard = document.createElement("div");
+        newCard.className = "card";
+
+        let bookTitle = document.createElement("p");
+        bookTitle.className = "card-text-content";
+        bookTitle.textContent = book.title;
+
+        let bookAuthor = document.createElement("p");
+        bookAuthor.className = "card-text-content";
+        bookAuthor.textContent = book.author;
+
+        let bookPages = document.createElement("p");
+        bookPages.className = "card-text-content";
+        bookPages.textContent = book.pages;
+        
+        let readStatus = document.createElement("p");
+        readStatus.className = "card-text-content";
+        readStatus.textContent = "Read: " + book.read;
+
+        newCard.appendChild(bookTitle);
+        newCard.appendChild(bookAuthor);
+        newCard.appendChild(bookPages);
+        newCard.appendChild(readStatus);
+
+        cardContainer.appendChild(newCard);
+
+    });
+}
+
+
 
 let book1 = new Book("Star Wars - Empire Strikes Back", "George Lucas", "400", true);
 let book2 = new Book("Cat in the Hat", "Dr. Seuss", "40", false);
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+
+displayBooks();
+
+console.log(myLibrary);
